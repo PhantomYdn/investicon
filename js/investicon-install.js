589807769403f948625dadf2b6198a92cb264801
Ilia Naryzhny <phantom@ydn.ru>
2015-11-08 08:26:19 -0800

    Add samples

--- js/investicon-install.js
+++ js/investicon-install.js
@@ -2,9 +2,9 @@ var icUrl = ""; //That all are loading from localhost, but in reality we should 
 
 document.write("<div onclick=\"document.getElementById('ic-window').style.display='block'\" style='position: fixed;top: 100px;right: 0;width: 90px;height: 90px;'>");
 document.write("  <a href=\"#\" onclick=\"document.getElementById('ic-window').style.display='block'; return false;\">");
-document.write("    <img src=\""+icUrl+"/images/icon.png\">");
+document.write("    <img src=\""+icUrl+"/images/icon.png\" style='max-width:100%'>");
 document.write("  </a>");
 document.write("</div>");
-document.write("<div id=\"ic-window\" style='display: none;position: fixed;top: 100px;right: 120px;'>");
+document.write("<div id=\"ic-window\" style='display: none;position: fixed;top: 100px;right: 120px; z-index: 10000'; border: 1px solid grey;>");
 document.write("  <iframe width=\"600px\" height=\"400px\" src=\""+icUrl+"/investicon.html\"></iframe>");
 document.write("</div>");
